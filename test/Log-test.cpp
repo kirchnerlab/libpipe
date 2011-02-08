7d4527a5c7ca780d9dede738b85a8744baf8f688
Marc Kirchner <mail@marc-kirchner.de>
2011-02-07 23:59:09 -0500

    added logging tests

--- test/Log-test.cpp
+++ test/Log-test.cpp
@@ -1,39 +1,102 @@
-/*
+/* 
  * Log-test.cpp
  *
- * Copyright (c) 2011 <+author+>
+ * Copyright (c) 2011 Marc Kirchner
+ * Copyright (c) 2009 Bernhard Kausler
  *
  */
 
+#include <cstring>
 #include <iostream>
+#include <ostream>
+#include <cstdio>
+
+#include <libpipe/Log.hpp>
 #include "vigra/unittest.hxx"
 
+using namespace libpipe;
 
-/** <+Short description of the test suite+>
- * <+Longer description of the test suite+> 
- */
 struct LogTestSuite : vigra::test_suite {
-    /** Constructor.
-     * The LogTestSuite constructor adds all Log tests to
-     * the test suite. If you write an additional test, add the test
-     * case here.
-     */
-    LogTestSuite() : vigra::test_suite("Log") {
-        add(testCase(&LogTestSuite::fail));
+    LogTestSuite() : vigra::test_suite("Logging") {
+        add( testCase(&LogTestSuite::testFILELog) );
+        add( testCase(&LogTestSuite::testOutput2FILE) );
+        add( testCase(&LogTestSuite::testMacros) );
+        add( testCase(&LogTestSuite::testNowTime) );
     }
 
-    /** Test that is guaranteed to fail.
-     * Leave this in until the complete Log class has tests.
-     */
-    void fail() {
-        failTest("No unit tests for class Log!");
+    void testOutput2FILE() {
+        Output2FILE out;
+        // getStream()
+        should(out.getRedirect() == stderr);
+        // output()
+        out.output("Some logging message");
+    }
+
+    // implicitly, this tests the Log<T> too
+    void testFILELog() {
+        FILELog l;
+
+        shouldEqual(l.fromString("DEBUG4"), logDEBUG4);
+        shouldEqual(l.fromString("DEBUG3"), logDEBUG3);
+        shouldEqual(l.fromString("DEBUG2"), logDEBUG2);
+        shouldEqual(l.fromString("DEBUG1"), logDEBUG1);
+        shouldEqual(l.fromString("DEBUG"), logDEBUG);
+        shouldEqual(l.fromString("INFO"), logINFO);
+        shouldEqual(l.fromString("WARNING"), logWARNING);
+        shouldEqual(l.fromString("ERROR"), logERROR);
+        shouldEqual(l.fromString("NO_LOGGING"), logNO_LOGGING);
+        shouldEqual(l.fromString("something else"), logINFO);
+
+        should(l.toString(logDEBUG4) == "DEBUG4");
+        should(l.toString(logDEBUG3) == "DEBUG3");
+        should(l.toString(logDEBUG2) == "DEBUG2");
+        should(l.toString(logDEBUG1) == "DEBUG1");
+        should(l.toString(logDEBUG) == "DEBUG");
+        should(l.toString(logINFO) == "INFO");
+        should(l.toString(logWARNING) == "WARNING");
+        should(l.toString(logERROR) == "ERROR");
+        should(l.toString(logNO_LOGGING) == "NO_LOGGING");
+        // invalid input
+        should(l.toString((LogLevel)(-1)) == "INFO");
+        // getReportingLevel()
+        shouldEqual(l.getReportingLevel(), logDEBUG4);
+        // get()
+        l.get();
+        l.get(logDEBUG4);
+        l.get(logDEBUG3);
+        l.get(logDEBUG2);
+        l.get(logDEBUG1);
+        l.get(logDEBUG);
+        l.get(logINFO);
+        l.get(logWARNING);
+        l.get(logERROR);
+        l.get(logNO_LOGGING); // should default to INFO
+        l.get((LogLevel)(-1)); // invalid input
+        // test intended usage
+        FILELog().get() << "Test of intended usage";
+    }
+
+    void testMacros() {
+        // LIBPIPE_LOG
+        LIBPIPE_LOG(logINFO) << "Macro test.";
+
+        // max level mechanism
+#undef FILELOG_MAX_LEVEL
+#define FILELOG_MAX_LEVEL logINFO
+        LIBPIPE_LOG(logWARNING) << "This should be logged, since WARNING is above INFO.";
+        LIBPIPE_LOG(logDEBUG) << "This SHOULDN'T be logged, since DEBUG is below INFO!";
+
+        // reset to standard max level
+#undef FILELOG_MAX_LEVEL
+#define FILELOG_MAX_LEVEL logDEBUG4
+        LIBPIPE_LOG(logDEBUG1) << "This should be logged again, since we reset to logDEBUG4.";
+    }
+
+    void testNowTime() {
+        nowTime();
     }
 };
 
-
-/** The main function that runs the tests for class Log.
- * Under normal circumstances you need not edit this.
- */
 int main()
 {
     LogTestSuite test;
@@ -41,4 +104,5 @@ int main()
     std::cout << test.report() << std::endl;
     return success;
 }
+
 
