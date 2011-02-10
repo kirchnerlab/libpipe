18a108d9a94f1bc85c23aee19664b25ef3af9870
Marc Kirchner <mail@marc-kirchner.de>
2011-02-10 01:03:13 -0500

    moved Exception implementation into cpp file

--- include/libpipe/Exception.hpp
+++ include/libpipe/Exception.hpp
@@ -23,35 +23,29 @@ class Exception : public std::exception
      *                 Hence, responsibility for deleting the \c char* lies
      *                 with the caller. 
      */
-    explicit Exception(const char* message) {
-        what_ = message;
-    }
+    explicit Exception(const char* message);
     
     /** Constructor (C++ STL strings).
      *  @param message The error message.
      */
-    explicit Exception(const std::string& message) {
-        what_ = message;
-    }
+    explicit Exception(const std::string& message);
     
     /** Destructor.
      * Virtual to allow for subclassing.
      */
-    virtual ~Exception() throw() {}
+    virtual ~Exception() throw();
 
     /** Returns a pointer to the (constant) error description.
      *  @return A pointer to a \c const \c char*. The underlying memory
      *          is in posession of the \c Exception object. Callers \a must
      *          not attempt to free the memory.
      */
-    virtual const char * what() const throw() {
-        return what_.c_str();
-    }
+    virtual const char* what() const throw();
     
   protected:
     /** Error message.
      */
-    std::string what_;
+    std::string msg_;
 };
 
 /** Convenience macros.
