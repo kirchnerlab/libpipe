8fc98cd377d67153d0a5cf1207b0ca506808eb89
Marc Kirchner <mail@marc-kirchner.de>
2011-02-10 01:14:14 -0500

    adding the implementation helps.

---
+++ src/Exception.cpp
@@ -0,0 +1,26 @@
+/*
+ * Exception.cpp
+ *
+ * Copyright (c) 2011 Marc Kirchner
+ *
+ */
+
+#include <libpipe/config.hpp>
+#include <libpipe/Exception.hpp>
+
+using namespace libpipe;
+
+Exception::Exception(const char* message) : msg_(message)
+{}
+
+Exception::Exception(const std::string& message) : msg_(message)
+{}
+
+Exception::~Exception() throw()
+{}
+
+const char* Exception::what() const throw()
+{
+    return msg_.c_str();
+}
+
