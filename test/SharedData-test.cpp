18b63f93c73e0a916d4df3aa51b66973891840e8
Marc Kirchner <mail@marc-kirchner.de>
2011-02-19 11:34:50 -0500

    Adding the SharedData template; this allows data sharing for non-default-constructible data.

---
+++ test/SharedData-test.cpp
@@ -0,0 +1,51 @@
+/*
+ * SharedData-test.cpp
+ *
+ * Copyright (c) 2011 Marc Kirchner
+ *
+ */
+
+#include <iostream>
+#include "vigra/unittest.hxx"
+
+#include <libpipe/SharedData.hpp>
+
+using namespace libpipe;
+
+/** Test suite for the SharedData template class
+ */
+struct SharedDataTestSuite : vigra::test_suite {
+    /** Constructor.
+     * The SharedDataTestSuite constructor adds all SharedData tests to
+     * the test suite. If you write an additional test, add the test
+     * case here.
+     */
+    SharedDataTestSuite() : vigra::test_suite("SharedData") {
+        add(testCase(&SharedDataTestSuite::instance));
+    }
+
+    void instance() {
+        SharedData<int> s;
+        shouldEqual(s.isNull(), true);
+        SharedData<int> s2(new int);   
+        shouldEqual(s2.isNull(), false);
+        *(s2.get()) = 10;
+        int* pI = new int(5);
+        s.set(pI);
+        shouldEqual(s.get(), pI);
+        shouldEqual(*(s.get()), 5);
+    }
+};
+
+
+/** The main function that runs the tests for class SharedData.
+ * Under normal circumstances you need not edit this.
+ */
+int main()
+{
+    SharedDataTestSuite test;
+    int success = test.run();
+    std::cout << test.report() << std::endl;
+    return success;
+}
+
