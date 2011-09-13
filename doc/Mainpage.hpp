e8bb3e10e105b8953e7c9fd43927157fc222a748
Marc Kirchner <mail@marc-kirchner.de>
2011-09-13 08:50:04 +0200

    Added basic main page docs

---
+++ doc/Mainpage.hpp
@@ -0,0 +1,18 @@
+/*!
+
+\mainpage libpipe: a lightweight C++ pipelining library
+
+\section sec_intro Introduction
+
+\section sec_install Installation
+
+\section Usage
+
+\section sec_examples Examples
+
+\section sec_appendix Appendix
+
+\subsection sec_issues Known Issues
+
+*/
+
