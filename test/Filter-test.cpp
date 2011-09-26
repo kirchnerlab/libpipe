4f064c3b476caa6c6225f1d43c743fb975730050
David Sichau <mail@sichau.eu>
2011-09-26 11:07:06 -0400

    Added test for the request switch in the filter.

--- test/Filter-test.cpp
+++ test/Filter-test.cpp
@@ -23,60 +23,79 @@ using namespace libpipe;
  */
 struct FilterTestSuite : vigra::test_suite
 {
-    /** Constructor.
-     * The FilterTestSuite constructor adds all Filter tests to
-     * the test suite. If you write an additional test, add the test
-     * case here.
-     */
-    FilterTestSuite() :
-        vigra::test_suite("Filter")
-    {
-        add(testCase(&FilterTestSuite::testConstructionAndDestruction));
-        add(testCase(&FilterTestSuite::testCallChain));
-        add(testCase(&FilterTestSuite::testName));
-    }
-
-    void testConstructionAndDestruction()
-    {
-        // this is a test for valgrind
-        typedef BasicFilter<Identity, TestManager> IdentityFilter;
-        IdentityFilter filter("bla");
-    }
-
-    void testCallChain()
-    {
-        // tests if the filter calls its managers process request function
-        // the manager will then call the process request function of the fail
-        // filter and raise an exception.
-        typedef BasicFilter<RaiseExceptionAlg, TestManager> FailFilter;
-        Filter* f = new FailFilter("Fail!");
-        Request req(Request::UPDATE);
-        bool thrown = false;
-        try {
-            req = f->processRequest(req);
-        } catch (RequestException& e) {
-            thrown = true;
+        /** Constructor.
+         * The FilterTestSuite constructor adds all Filter tests to
+         * the test suite. If you write an additional test, add the test
+         * case here.
+         */
+        FilterTestSuite() :
+                vigra::test_suite("Filter")
+        {
+            add(testCase(&FilterTestSuite::testConstructionAndDestruction));
+            add(testCase(&FilterTestSuite::testCallChain));
+            add(testCase(&FilterTestSuite::testName));
         }
-        shouldEqual(thrown, true);
-        delete f;
-    }
 
-    void testName()
-    {
-        // getset name
-        typedef BasicFilter<RaiseExceptionAlg, TestManager> FailFilter;
-        Filter* f = new FailFilter("Fail!");
-        f->setName(f->getName());
-        shouldEqual(strncmp(f->getName().c_str(), "Fail!", 5), 0);
-        f->setName("foo");
-        shouldEqual(strncmp(f->getName().c_str(), "foo", 3), 0);
-        delete f;
-    }
+        void testConstructionAndDestruction()
+        {
+            // this is a test for valgrind
+            typedef BasicFilter<Identity, TestManager> IdentityFilter;
+            IdentityFilter filter("bla");
+        }
 
-    /*
-     * tests for get/setAlgorithm and get/setManager are part
-     * of the BasicFilter<A, M> test routines.
-     */
+        void testCallChain()
+        {
+            // tests if the filter calls its managers process request function
+            // the manager will then call the process request function of the fail
+            // filter and raise an exception.
+            typedef BasicFilter<RaiseExceptionAlg, TestManager> FailFilter;
+            Filter* f = new FailFilter("Fail!");
+            Request req(Request::UPDATE);
+            bool thrown = false;
+            try {
+                req = f->processRequest(req);
+            } catch (RequestException& e) {
+                thrown = true;
+            }
+            shouldEqual(thrown, true);
+
+            Request req1(Request::DELETE);
+            thrown = false;
+            try {
+                req1 = f->processRequest(req1);
+            } catch (RequestException& e) {
+                thrown = true;
+            }
+            shouldEqual(thrown, false);
+
+            Request req2(Request::Type(3));
+            thrown = false;
+            try {
+                req2 = f->processRequest(req2);
+            } catch (RequestException& e) {
+                thrown = true;
+            }
+            shouldEqual(thrown, true);
+
+            delete f;
+        }
+
+        void testName()
+        {
+            // getset name
+            typedef BasicFilter<RaiseExceptionAlg, TestManager> FailFilter;
+            Filter* f = new FailFilter("Fail!");
+            f->setName(f->getName());
+            shouldEqual(strncmp(f->getName().c_str(), "Fail!", 5), 0);
+            f->setName("foo");
+            shouldEqual(strncmp(f->getName().c_str(), "foo", 3), 0);
+            delete f;
+        }
+
+        /*
+         * tests for get/setAlgorithm and get/setManager are part
+         * of the BasicFilter<A, M> test routines.
+         */
 
 };
 
