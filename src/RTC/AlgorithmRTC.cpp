8a121c897296559dfa334aac4f83b3787481e2b0
David Sichau <mail@sichau.eu>
2011-10-06 11:42:18 -0400

    Added destructor to Filter

---
+++ src/RTC/AlgorithmRTC.cpp
@@ -0,0 +1,109 @@
+/*
+ * Algorithm.cpp
+ * 
+ * Copyright (c) 2010 Marc Kirchner
+ *               2011 David Sichau
+ */
+#include <libpipe/RTC/AlgorithmRTC.hpp>
+#include <cstring> // for memset
+#include <sys/time.h> // for gettimeofday
+#include <limits>
+#include <libpipe/Log.hpp>
+
+#undef LIBPIPE_FILELOG_MAX_LOGGING_LEVEL
+#define LIBPIPE_FILELOG_MAX_LOGGING_LEVEL libpipe::logINFO
+
+bool operator<=(const timeval& lhs, const timeval& rhs)
+{
+    if (lhs.tv_sec < rhs.tv_sec || (lhs.tv_sec == rhs.tv_sec && lhs.tv_usec
+            <= rhs.tv_usec)) {
+        return true;
+    }
+    return false;
+}
+
+bool operator==(const timeval& lhs, const timeval& rhs)
+{
+    if (lhs.tv_sec == rhs.tv_sec && lhs.tv_usec == rhs.tv_usec)
+        return true;
+    return false;
+}
+
+std::ostream& operator<<(std::ostream& os, const timeval& tv)
+{
+    os << tv.tv_sec << "." << tv.tv_usec;
+    return os;
+}
+
+using namespace libpipe_rtc;
+
+timeval AlgorithmRTC::initMaxTime()
+{
+    timeval tv;
+    tv.tv_sec = std::numeric_limits<time_t>::max();
+    tv.tv_usec = std::numeric_limits<suseconds_t>::max();
+    return tv;
+}
+
+timeval AlgorithmRTC::initMinTime()
+{
+    timeval tv;
+    tv.tv_sec = std::numeric_limits<time_t>::min();
+    tv.tv_usec = std::numeric_limits<suseconds_t>::min();
+    return tv;
+}
+
+// initialize with maximum value such that any call to update will make the
+// instance "younger" than any non-updated instances
+const timeval AlgorithmRTC::MAX_TIME = AlgorithmRTC::initMaxTime();
+
+// initialize with minimum value such that any call to update will make the
+// instance "older" than any non-updated instances
+const timeval AlgorithmRTC::MIN_TIME = AlgorithmRTC::initMinTime();
+
+AlgorithmRTC::AlgorithmRTC() :
+    mTime_(AlgorithmRTC::MAX_TIME)
+{
+}
+
+AlgorithmRTC::~AlgorithmRTC()
+{
+}
+
+libpipe::Request& AlgorithmRTC::processRequest(libpipe::Request& req)
+{
+    req = this->update(req);
+    if(req.is(libpipe::Request::DELETE)){
+        this->setMTime(AlgorithmRTC::MIN_TIME);
+    }
+    return req;
+}
+
+const timeval& AlgorithmRTC::getMTime() const
+{
+    return mTime_;
+}
+
+void AlgorithmRTC::setMTime(const timeval& mTime)
+{
+    mTime_ = mTime;
+}
+
+void AlgorithmRTC::updateMTime()
+{
+    // use gettimeofday for microsecond resolution
+    gettimeofday(&mTime_, NULL);
+}
+
+bool AlgorithmRTC::needUpdate() const
+{
+    /*
+     LIBPIPE_LOG(libpipe::logDEBUG) << "Comparing timevals: " << "[" << mTime_.tv_sec << "."
+     << mTime_.tv_usec << "] vs. [" << Algorithm::MAX_TIME.tv_sec << "."
+     << Algorithm::MAX_TIME.tv_usec;
+     */
+    return mTime_ == AlgorithmRTC::MAX_TIME;
+}
+
+const bool AlgorithmRTC::registered_ = registerLoader();
+
---
+++ src/RTC/ManagerRTC.cpp
@@ -0,0 +1,78 @@
+/*
+ * Manager.cpp
+ *
+ * Copyright (c) 2010 Marc Kirchner
+ *               2011 David Sichau
+ *
+ */
+#include <libpipe/Manager.hpp>
+#include <libpipe/Filter.hpp>
+#include <libpipe/Algorithm.hpp>
+
+using namespace libpipe;
+
+Manager::Manager() :
+        algorithm_(0)
+{
+}
+
+Manager::~Manager()
+{
+}
+
+Algorithm* Manager::getAlgorithm()
+{
+    return algorithm_;
+}
+
+void Manager::setAlgorithm(Algorithm* alg)
+{
+    // the manager does not own the
+    // algorithm object. Hence, just move the pointer.
+    algorithm_ = alg;
+}
+
+Request& Manager::processRequest(Request& req)
+{
+    if (req.is(Request::UPDATE)) {
+        if (!algorithm_) {
+            throw RequestException(
+                "Cannot process request. No algorithm setup available.");
+        }
+        typedef FilterSet::iterator MSI;
+        // iterate over all sources
+        for (MSI i = sources_.begin(); i != sources_.end(); ++i) {
+            try {
+                req = (*i)->processRequest(req);
+            } catch (RequestException& e) {
+                throw;
+            }
+        }
+        try {
+            req = algorithm_->processRequest(req);
+        } catch (std::exception& e) {
+            std::string str(e.what());
+            throw RequestException(
+                "ModificationTimeManager: Cannot process request: algorithm execution caused exception: "
+                        + str);
+        } catch (...) {
+            throw RequestException(
+                "ModificationTimeManager: Cannot process request: algorithm execution caused exception.");
+        }
+    } else if (req.is(Request::DELETE)) {
+        req = algorithm_->processRequest(req);
+        this->disconnect();
+    }
+    return req;
+}
+
+void Manager::connect(boost::shared_ptr<Filter> f)
+{
+    sources_.insert(f);
+}
+
+void Manager::disconnect()
+{
+    sources_.clear();
+}
+
