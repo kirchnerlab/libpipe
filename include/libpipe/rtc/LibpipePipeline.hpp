b589f4ab014ace88d2ed1b9e6af0392f3df4f31d
David Sichau <mail@sichau.eu>
2011-10-17 10:41:20 -0400

    Added Pipeline Object to provide all features to run the whole pipeline.

--- include/libpipe/rtc/LibpipePipeline.hpp
+++ include/libpipe/rtc/LibpipePipeline.hpp
@@ -8,14 +8,36 @@
 #ifndef LIBPIPEPIPELINE_HPP_
 #define LIBPIPEPIPELINE_HPP_
 
+#include "libpipe/Request.hpp"
+#include "libpipe/rtc/Filter.hpp"
+
+#include <queue>
+
+#include <boost/shared_ptr.hpp>
+
 namespace libpipe {
 namespace rtc {
+
+
 
 class LibpipePipeline
 {
     public:
         LibpipePipeline();
         ~LibpipePipeline();
+
+        void push(Request const & req, boost::shared_ptr<Filter> filter);
+
+        void run();
+
+        void getTrace(std::vector<std::string>& trace);
+
+    private:
+        std::queue<boost::shared_ptr<Filter> > pipelineQueue_;
+
+        std::queue<Request> requestQueue_;
+
+       std::vector<std::string> trace_;
 };
 
 } /* namespace rtc */
--- src/rtc/LibpipePipeline.cpp
+++ src/rtc/LibpipePipeline.cpp
@@ -12,13 +12,45 @@ namespace rtc {
 
 LibpipePipeline::LibpipePipeline()
 {
-    // TODO Auto-generated constructor stub
-
 }
 
 LibpipePipeline::~LibpipePipeline()
 {
-    // TODO Auto-generated destructor stub
+}
+
+void LibpipePipeline::push(Request const & req,
+    boost::shared_ptr<Filter> filter)
+{
+    pipelineQueue_.push(filter);
+    requestQueue_.push(req);
+}
+
+void LibpipePipeline::getTrace(std::vector<std::string>& trace)
+{
+    trace = trace_;
+}
+
+void LibpipePipeline::run()
+{
+
+    while (!pipelineQueue_.empty()) {
+        Request tempReq = requestQueue_.front();
+
+        LIBPIPE_REQUEST_TRACE(tempReq, "Starting.");
+        try {
+            pipelineQueue_.front()->getManager()->processRequest(tempReq);
+
+        } catch (libpipe::RequestException& e) {
+            std::cerr << e.what() << std::endl;
+        }
+        std::vector<std::string> tempTrace;
+        tempReq.getTrace(tempTrace);
+        trace_.insert(trace_.end(), tempTrace.begin(), tempTrace.end() );
+
+        pipelineQueue_.pop();
+        requestQueue_.pop();
+    }
+
 }
 
 } /* namespace rtc */
