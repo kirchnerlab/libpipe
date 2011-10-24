8b5b887e5f5e35523b0fd5d48e841a5c5428b5c2
David Sichau <mail@sichau.eu>
2011-10-24 14:03:14 -0400

    try out to get memtest running on jenkins

--- test/memtest.py
+++ test/memtest.py
@@ -21,7 +21,7 @@ def garbage(line):
 def memcheck(test):
     ''' run valgrind-memcheck on test in testdir. return xml output as string '''
     v = find_valgrind()
-    cmd = v+" --tool=memcheck --child-silent-after-fork=yes --leak-check=full --xml=yes --xml-fd=3 --num-callers=50 " + test + " 3>memcheck.tmp"
+    cmd = "valgrind --tool=memcheck --child-silent-after-fork=yes --leak-check=full --xml=yes --xml-fd=3 --num-callers=50 " + test + " 3>memcheck.tmp"
     #print cmd
     system(cmd)
     out = open("memcheck.tmp").readlines()
