df80228c631dbb00006ff8816777fa790a5c6457
David Sichau <mail@sichau.eu>
2011-10-24 15:38:53 -0400

    test for rikku

--- test/memtest.py
+++ test/memtest.py
@@ -20,9 +20,9 @@ def garbage(line):
 
 def memcheck(test):
     ''' run valgrind-memcheck on test in testdir. return xml output as string '''
-#    v = find_valgrind()
-#    cmd = v+" --tool=memcheck --child-silent-after-fork=yes --leak-check=full --xml=yes --xml-fd=3 --num-callers=50 " + test + " 3>memcheck.tmp"
-    cmd = "/usr/local/bin/valgrind --tool=memcheck --child-silent-after-fork=yes --leak-check=full --xml=yes --xml-fd=3 --num-callers=50 " + test + " 3>memcheck.tmp"
+    v = find_valgrind()
+    cmd = v+" --tool=memcheck --child-silent-after-fork=yes --leak-check=full --xml=yes --xml-fd=3 --num-callers=50 " + test + " 3>memcheck.tmp"
+#    cmd = "/usr/local/bin/valgrind --tool=memcheck --child-silent-after-fork=yes --leak-check=full --xml=yes --xml-fd=3 --num-callers=50 " + test + " 3>memcheck.tmp"
 
     #print cmd
     system(cmd)
